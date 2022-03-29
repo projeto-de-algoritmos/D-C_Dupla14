021b42ebe7f1775674f38f336d0ec503bfb6bf04
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-03-28 21:11:00 -0300

    Adicionando comentarios no sortArray

--- src/sortAnArray.py
+++ src/sortAnArray.py
@@ -9,6 +9,7 @@ def sortarray(nums):
             i = l
             j = m + 1
 
+            # verifica do inicio ate o fim
             while i <= m and j <= r:
                 if a[i] < a[j]:
                     sorted[k] = a[i]
@@ -19,11 +20,13 @@ def sortarray(nums):
                     k += 1
                     j += 1
 
+            # verifica do inicio ate o meio
             while i <= m:
                 sorted[k] = a[i]
                 k += 1
                 i += 1
-
+            
+            # verifica do meio ate o ate o fim
             while j <= r:
                 sorted[k] = a[j]
                 k += 1
@@ -31,15 +34,19 @@ def sortarray(nums):
 
             a[l:l + len(sorted)] = sorted
 
+        # meio
         m = (l + r) // 2
+        # chama a funca para as duas metades
         mergeSort(a, l, m)
         mergeSort(a, m + 1, r)
+        # combina as duas formas de maneira ordenada
         merge(a, l, m, r)
 
     mergeSort(nums, 0, len(nums) - 1)
     
     return nums
 
+# listas
 nums = [5,2,3,1]
 # nums = [5,1,1,2,0,0]
 
