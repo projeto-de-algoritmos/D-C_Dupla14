b50a5ffb899ad557cb46f07ef12803af0ba4b510
Nathalia Lorena <nathalialorenacardoso@gmail.com>
2022-03-28 22:12:41 -0300

    Acrescentando comentários

--- src/theSkylineProblem.py
+++ src/theSkylineProblem.py
@@ -1,31 +1,31 @@
 def skyline(predios):
-    n = len(predios)
+    n = len(predios) # Retorna o número de itens da lista
     if n == 0:
         return []
     if n == 1:
-        L, R, altura = predios[0]
+        L, R, altura = predios[0] # L: ponto à esquerda; R: ponto à direita; altura do prédio
         return [[L, altura], [R, 0]]
 
-    L = skyline(predios[:n // 2])
+    L = skyline(predios[:n // 2]) 
     R = skyline(predios[n // 2:])
    
-    return merge(L, R)
+    return merge(L, R) # Chamada para função mergesort
 
-def merge(L, R):
+def merge(L, R): # Função mergesort
     lista = []
-    i = 0  
-    j = 0  
+    i = 0  # Índice da esquerda
+    j = 0  # Índice da direita
     LY = 0
     RY = 0
 
     while i < len(L) and j < len(R):
        
         if L[i][0] < R[j][0]:
-            LY = L[i][1]  
+            LY = L[i][1]  # Atualizando o progresso da esquerda
             addPonto(lista, L[i][0], max(L[i][1], RY))
             i += 1
         else:
-            RY = R[j][1]  
+            RY = R[j][1]  # Atualizando o progresso da direita
             addPonto(lista, R[j][0], max(R[j][1], LY))
             j += 1
 
@@ -39,7 +39,7 @@ def merge(L, R):
 
     return lista
 
-def addPonto(lista, x, y):
+def addPonto(lista, x, y): # Acrescentando mais pontos na lista
     if lista and lista[-1][0] == x:
         lista[-1][1] = y
         return
@@ -47,6 +47,7 @@ def addPonto(lista, x, y):
         return
     lista.append([x, y])
 
+# Casos de teste
 
 predios = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
 # predios = [[0,2,3],[2,5,3]]
